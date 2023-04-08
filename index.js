3300a4b2322392d031bf9507e5fc34bef6fd29ca
vivek <vivuchat84@gmail.com>
2023-04-08 10:05:14 +0530

    added cors headers

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
+// use cors headers
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+
+
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
